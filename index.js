54932bc7e618810a7beda0546bbdc245de724086
Ann-Helen Lausmaa <code@ahlvdesign.se>
2022-09-04 09:19:11 +0200

    almost finished

---
+++ index.js
@@ -0,0 +1,26 @@
+let menuOpen = false;
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".mobile-menu");
+const links = document.getElementsByClassName("mobile-link");
+const documentBody = document.body;
+
+const toggleMenu = () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+    menu.style.right = "0%";
+    documentBody.style.overflow = "hidden";
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    menu.style.right = "-100%";
+    documentBody.style.overflow = "auto";
+  }
+}
+
+window.addEventListener("load", function () {
+  menuBtn.addEventListener("click", toggleMenu);
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", toggleMenu);
+  }
+});
